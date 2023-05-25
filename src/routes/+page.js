c877fa94bdd370bcbd91f267ea6c743f416b99c3
sondreaa <104071423+sondreaa@users.noreply.github.com>
2023-05-25 09:49:12 +0200

    css

---
+++ src/routes/+page.js
@@ -0,0 +1,30 @@
+import sanityClient from "@sanity/client";
+
+const client = sanityClient({
+projectId: "a70jffeo",
+dataset: "production",
+apiVersion: "2021-10-21",
+useCdn: false
+});
+
+export async function load() {
+
+    const data = await client.fetch(
+        `*[_type == "periodesystem"] | order(atomnummer asc)`
+        );
+    
+    if (data) {
+    
+        return {
+        periodesystem: data
+        
+        };
+    }
+    return {
+        status: 500,
+        body: new Error("Internal Server Error")
+    };
+
+}
+
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,80 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<head>
+    <title>Grunnstoffspeideren</title>
+</head>
+
+<div id="overskrift">
+    <h1>Grunnstoffspeideren</h1>
+</div>
+    
+
+
+<div id="elbox">
+    
+    <div id="atomnum">
+        <p>3</p>
+    </div>
+
+    <div id="masse">
+        <p>6.941</p>
+    </div>
+    
+    <div id="symbol">
+        <p>Li</p>
+    </div>
+
+    <div id="navn">
+        <p>Litium</p>
+    </div>
+
+</div>
+
+<style>
+    /* div{
+        border: 1px solid red;
+    } */
+    #elbox{
+        width: 150px;
+        height: 160px;
+        position: relative;
+        border: 3px solid black;
+
+        background-color: lightblue;
+
+
+    }
+    #atomnum{
+        position: absolute;
+        left: 5%;
+        font-size: 200%;
+        transform: translate(0%, -30%);
+    }
+    #masse{
+        position: absolute;
+        right: 5%;
+        transform: translate(0%, -15%);
+    }
+    #symbol{
+        position: absolute;
+        transform: translate(-50%, -50%);
+        left: 50%;
+        top: 50%;
+
+        font-size: 350%;
+    }
+    #navn{
+        position: absolute;
+        bottom: 0%;
+        left: 50%;
+        transform: translate(-50%, 0%);
+    }
+
+</style>
+
+
+
+
+<script>
+    export let data;
+
+    console.log(data.periodesystem)
+</script>
